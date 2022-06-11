99bb70737630ffd62a3049cb5e2cf532bd9f09d4
Alastair Farman <alastair.farman@gmail.com>
2022-06-11 21:51:26 +0100

    Game works with no stop or replay option. CSS done but finishing touches to be added alater.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-;
-
-
 
  const options = ['Rock', 'Scissors', 'Paper'];
  let playerScore = 0
@@ -11,61 +8,80 @@ function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3);
     return options[randomNumber];
 }
-/* Player enters option*/
-function playerPlay() {
-    let playerChoice = prompt('Rock, Paper or Scissors?')
-    return playerChoice;
-}
+
+/* Button functionality */
+
+ const btn = document.querySelectorAll('.Button').forEach((btn) => {
+    btn.addEventListener('click', () => {
+        playRound(btn.id,computerPlay());
+    });
+});
+
 /* One round of Rock, Paper, Scissors */
-function playRound() {
-        let computerSelection = computerPlay()
-        let playerSelection = playerPlay()
+function playRound(playerSelection, computerSelection) {
+
     console.log(computerSelection)
-    console.log(playerSelection)  
+    console.log(playerSelection)
+    console.log(playerScore)
+    let resultDeclaration = ""  
     
     if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'Scissors') {
-        console.log('You win!');
-        playerScore++;
+        resultDeclaration = "Your rock blunts the computers scissors";
+        playerScore++; console.log(playerScore);
     } else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'Paper') {
-        console.log('You lose!');
+        resultDeclaration = "The computer carefully wraps your rock with paper";
         computerScore++;
     } else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection === 'Rock') {
-        console.log('Draw!');
+        resultDeclaration = "Two rocks pointlessly bump together";
     } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'Rock') {
-        console.log('You win!');
+        resultDeclaration = "Your paper envelops the computer's useless rock";
         playerScore++;
     } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'Scissors') {
-        console.log('You lose!');
+        resultDeclaration = "The computer takes a pair of scissors and splits your paper in two";
         computerScore++;
     } else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection === 'Paper') {
-        console.log('Draw!');
+        resultDeclaration = "You and the computer create a pile";
     } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'Paper') {
-        console.log('You win!');
+        resultDeclaration = "You easily snip the computers paper";
         playerScore++;
     } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'Rock') {
-        console.log('You lose!');
+        resultDeclaration = "The computer uses a rock to counter your cut";
         computerScore++;
     } else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection === 'Scissors') {
-        console.log('Draw!');
+        resultDeclaration = "You and the computer scissor each other";
         }
-}
-/* 5 round game of Rock, Paper, Scissors */
-function game () {
-    for (let i = 0; i < 5; i++) {
-        playRound()
-    }
-        console.log('Player: ' + playerScore)
-        console.log('Computer: ' + computerScore)
+        printResult(resultDeclaration)
 
-    if (playerScore > computerScore) {
-        console.log('You Win the Game!')
-    } else if (playerScore < computerScore) {
-        console.log('You Lose the Game!')
-    } else if (playerScore === 2 && computerScore === 2) {
-        console.log('It\'s a draw!')
-    }
 }
 
 
-game()
+/* Create element for displaying results and print function */
+const result = document.querySelector('#result');
+const content = document.createElement('div');
+const score = document.querySelector('#Score');
+
+function printResult(resultOutput) {
+    content.textContent = resultOutput;
+    result.appendChild(content);
+    score.innerText = playerScore + " - " + computerScore
+        if (playerScore >= 5) {
+            content.textContent = 'Congratulations!'
+            result.appendChild(content)
+            content.style.backgroundColor = '#121212';
+            content.style.color = '#F52F57';
+            content.style.lineHeight = 2;
+            content.style.fontSize = '200%';
+            
+        } else if (computerScore >= 5) {
+            content.textContent = 'Commiserations.'
+            result.appendChild(content)
+            content.style.backgroundColor = '#F52F57';
+            content.style.color = '#EDEDf4';
+            content.style.lineHeight = 2;
+            content.style.fontSize = '200%';
+        }
+
+}
+
+
 
